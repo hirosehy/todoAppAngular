759e1ed2cab1230846150f8e4aadce6447a1bc2d
hirosehy <hirose.hy@gmail.com>
2019-12-30 10:13:39 +0900

    update: todoコンポーネント作成 モック作成

---
+++ src/app/mock-todos.ts
@@ -0,0 +1,7 @@
+import { Todo } from './todo'
+
+export const TODOS: Todo[] = [
+  { content: "頑張る", created: new Date(2016, 2, 29), done: new Date(2016, 3, 2) },
+  { content: "やる", created: new Date(2017, 8, 29), done: new Date(2017, 9,7) },
+  { content: "遊ぶ", created: new Date(2018, 7, 15), done: new Date(2018, 7,16) }
+]
---
+++ src/app/todo.ts
@@ -0,0 +1,5 @@
+export class Todo {
+  content: string;
+  created: Date;
+  done: Date;
+}
