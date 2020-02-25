64722e06db54cbefc4329818c323e23cf25dd178
hirosehy <hirose.hy@gmail.com>
2020-02-25 18:38:18 +0900

    update: setTodos追加

--- src/services/todos.service.ts
+++ src/services/todos.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service'
-import { TODOS } from '../app/mock-todos'
 import { Todo } from '../app/todo'
 
 @Injectable({
@@ -10,12 +9,16 @@ export class TodosService {
 
   constructor(private cookieService: CookieService) { }
 
+  todos: Todo[] =
+    this.cookieService.get('todos')
+      && JSON.parse(this.cookieService.get('todos'))
+      || []
+
   getTodos(): Todo[] {
-    const todos = this.cookieService.get('todos')
-    if (!todos) {
-      this.cookieService.set('todos', JSON.stringify([]))
-      return []
-    }
-    return JSON.parse(todos)
+    return this.todos
+  }
+
+  setTodos(todos: Todo[]): void {
+    this.cookieService.set('todos', JSON.stringify(todos))
   }
 }
